4da6467c60838b7b71ce514098bb6005752016a8
Reggio <contact@graphandco.com>
2019-11-15 18:57:21 +0100

    JWT

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
@@ -67,14 +69,30 @@ router.post(
 
       await user.save();
 
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+
       //Return jsonwebtoken
-      res.send('Utilisateur enregistré');
+      //res.send('Utilisateur enregistré');
     } catch (err) {
       console.error(err.message);
-      res.statut(500).send('Server error');
+      res.statut(500).send('Erreur Serveur');
     }
 
-    res.send('Utilisateur route');
+    //res.send('Utilisateur route');
   }
 );
 
