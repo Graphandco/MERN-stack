53e9ac42c04691ea97eb01a72668dc48f1d4cd1d
Reggio <contact@graphandco.com>
2019-11-19 18:45:44 +0100

    Delete User

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -147,4 +147,19 @@ router.get('/user/:user_id', async (req, res) => {
   }
 });
 
+//@route    DELETE api/profile
+//@desc     Delete profile, user & posts
+//@access   Private
+
+router.delete('/', auth, async (req, res) => {
+  try {
+    await Profile.findOneAndRemove({ user: req.user.id });
+    await User.findOneAndRemove({ _id: req.user.id });
+    res.json({ msg: 'Utilisateur supprimé' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Erreur Serveur');
+  }
+});
+
 module.exports = router;
