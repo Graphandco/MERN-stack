3f10c5137b4668fae1728d8dd9edaf0b772dd573
Reggio <contact@graphandco.com>
2019-11-26 19:21:48 +0100

    Créer / Effacer Experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -147,7 +147,7 @@ router.get('/user/:user_id', async (req, res) => {
   }
 });
 
-//@route    DELETE api/profile
+//@route    PUT api/profile/experience
 //@desc     Delete profile, user & posts
 //@access   Private
 
@@ -159,6 +159,91 @@ router.delete('/', auth, async (req, res) => {
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Erreur Serveur');
+  }
+});
+
+//@route    DELETE api/profile
+//@desc     Ajout experience au profil
+//@access   Private
+
+router.put(
+  '/experience',
+  [
+    auth,
+    [
+      check('title', 'Titre requis')
+        .not()
+        .isEmpty(),
+      check('company', 'Entreprise requise')
+        .not()
+        .isEmpty(),
+      check('from', 'Date requise')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description
+    } = req.body;
+
+    const newExp = {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description
+    };
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+
+      profile.experience.unshift(newExp);
+
+      await profile.save();
+
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Erreur serveur');
+    }
+  }
+);
+
+//@route    DELETE api/profile/experience/:exp_id
+//@desc     Efface experience du profil
+//@access   Private
+
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    // Get remove index
+    const removeIndex = profile.experience
+      .map(item => item.id)
+      .indexOf(req.params.exp_id);
+
+    profile.experience.splice(removeIndex, 1);
+
+    await profile.save();
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Erreur serveur');
   }
 });
 
